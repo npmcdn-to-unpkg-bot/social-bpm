07a0adbcc0917f7c9f7e5ab268b585a2bcfa56e2
Krishan Dadlani <krishan_dadlani@mckinsey.com>
2016-07-31 20:23:45 -0400

    added namespacing for Jquery

--- client/src/app/common/nocache/social-bpm-widget.js
+++ client/src/app/common/nocache/social-bpm-widget.js
@@ -2,6 +2,7 @@ var social_bpm_widget_version = 1.01;
 
 var AWS_SERVER_URL = "http://ec2-52-37-118-158.us-west-2.compute.amazonaws.com/";
 
+document.write('<script type="text/javascript" src="client/vendor/jquery/v3.1.0/jquery.min.js?version=' + social_bpm_widget_version + '"></script>');
 document.write('<script type="text/javascript" src="client/vendor/angularjs/v1.5.8/angular.min.js?version=' + social_bpm_widget_version + '"></script>');
 document.write('<script type="text/javascript" src="client/src/app/social-bpm-main.js?version=' + social_bpm_widget_version + '"></script>');
 document.write('<script type="text/javascript" src="client/src/app/common/directives/social-bpm.js?version=' + social_bpm_widget_version + '"></script>');
