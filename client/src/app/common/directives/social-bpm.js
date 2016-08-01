2933151fb7733dbae868f1d4a437bef6bf9e6f94
Krishan Dadlani <krishan_dadlani@mckinsey.com>
2016-07-31 20:41:39 -0400

    added jq ui and current date

--- client/src/app/common/directives/social-bpm.js
+++ client/src/app/common/directives/social-bpm.js
@@ -11,6 +11,7 @@ socialBpmModule.directive(
       controller: function ($scope, $element, $attrs, socialBpmDataService) {
         var filterType = $attrs.filterType;
         console.log(filterType);
+        $scope.todayDate = socialBpmJQuery.datepicker.formatDate('dd-MM-yy', new Date());;
         // Calling social bpm data via ajax starts here
         socialBpmDataService
           .getSocialBpmData()
--- client/src/app/common/nocache/social-bpm-widget.js
+++ client/src/app/common/nocache/social-bpm-widget.js
@@ -2,10 +2,13 @@ var social_bpm_widget_version = 1.01;
 
 var AWS_SERVER_URL = "http://ec2-52-37-118-158.us-west-2.compute.amazonaws.com/";
 
-document.write('<script type="text/javascript" src="client/vendor/jquery/v3.1.0/jquery.min.js?version=' + social_bpm_widget_version + '"></script>');
+
 document.write('<script type="text/javascript" src="client/vendor/angularjs/v1.5.8/angular.min.js?version=' + social_bpm_widget_version + '"></script>');
+document.write('<script type="text/javascript" src="client/vendor/jquery/v3.1.0/jquery.min.js?version=' + social_bpm_widget_version + '"></script>');
+document.write('<script type="text/javascript" src="client/vendor/jquery-ui/v1.12.0/jquery-ui.min.js?version=' + social_bpm_widget_version + '"></script>');
 document.write('<script type="text/javascript" src="client/src/app/social-bpm-main.js?version=' + social_bpm_widget_version + '"></script>');
 document.write('<script type="text/javascript" src="client/src/app/common/directives/social-bpm.js?version=' + social_bpm_widget_version + '"></script>');
 document.write('<script type="text/javascript" src="client/src/app/services/data-service.js?version=' + social_bpm_widget_version + '"></script>');
+
 
 document.write('<link rel="stylesheet" href="client/src/css/template.css" />');
